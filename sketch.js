0f32b2e470f85ca11a5a0a8747723e3d395326d9
3melie <emelie.rohbeck@fh-potsdam.de>
2025-05-29 10:22:56 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,54 @@
 let mic, fft;
-let threshold = 0.1; // Lautstärkeschwelle
 let lasers = [];
+let threshold = 0.1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, 360, 100, 100, 100);
+
   mic = new p5.AudioIn();
   mic.start();
 
-  fft = new p5.FFT();
+  fft = new p5.FFT(0.8, 1024);
   fft.setInput(mic);
 
   background(0);
 }
 
 function draw() {
-  background(0, 20); // leicht transparent, um Spuren zu erzeugen
+  background(0, 20); // weiches Ausfaden
 
   let spectrum = fft.analyze();
   let vol = mic.getLevel();
 
   if (vol > threshold) {
-    let freqIndex = findDominantFreqIndex(spectrum);
-    let freq = freqIndexToFreq(freqIndex);
-    let angle = map(freqIndex, 0, spectrum.length, 0, TWO_PI);
+    let i = findDominantFreqIndex(spectrum);
+    let freq = indexToFreq(i, spectrum.length);
+    let angle = map(i, 0, spectrum.length, 0, TWO_PI);
     let len = map(vol, 0, 1, 50, 400);
-    
-    let x = random(width);
-    let y = random(height);
+
+    let pos = createVector(random(width), random(height));
     let dir = p5.Vector.fromAngle(angle).mult(len);
 
     lasers.push({
-      x: x,
-      y: y,
+      x: pos.x,
+      y: pos.y,
       dx: dir.x,
       dy: dir.y,
-      col: color(map(freq, 100, 10000, 0, 360), 100, 100)
+      hue: map(freq, 100, 10000, 0, 360)
     });
   }
 
-  // Zeichne alle Laser
-  colorMode(HSB, 360, 100, 100, 100);
+  // Zeige alle Laserstrahlen
   for (let l of lasers) {
-    stroke(l.col);
+    stroke(l.hue, 80, 100);
     strokeWeight(2);
     line(l.x, l.y, l.x + l.dx, l.y + l.dy);
+  }
+
+  // Optional: begrenze die Anzahl Laser
+  if (lasers.length > 200) {
+    lasers.splice(0, 1);
   }
 }
 
@@ -59,7 +64,12 @@ function findDominantFreqIndex(spectrum) {
   return index;
 }
 
-function freqIndexToFreq(index) {
+function indexToFreq(index, bins) {
   let nyquist = sampleRate() / 2;
-  return index * (nyquist / spectrum.length);
+  return index * (nyquist / bins);
+}
+
+function touchStarted() {
+  // Für Browser-Sicherheitsrichtlinien auf mobilen Geräten
+  getAudioContext().resume();
 }
